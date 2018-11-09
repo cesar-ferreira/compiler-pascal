39689d6f222f6c16f062ed16dc6e297fa20f7edd
Pedro H. Costa <pedro.maia@ccc.ufcg.edu.br>
2018-11-09 14:42:38 -0300

    iniciando...

---
+++ org.xtext.compiler.pascal.ide/src-gen/org/xtext/ide/contentassist/antlr/internal/InternalPascalParser.java
@@ -0,0 +1,600 @@
+package org.xtext.ide.contentassist.antlr.internal;
+
+import java.io.InputStream;
+import org.eclipse.xtext.*;
+import org.eclipse.xtext.parser.*;
+import org.eclipse.xtext.parser.impl.*;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.parser.antlr.XtextTokenStream;
+import org.eclipse.xtext.parser.antlr.XtextTokenStream.HiddenTokens;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.AbstractInternalContentAssistParser;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.DFA;
+import org.xtext.services.PascalGrammarAccess;
+
+
+
+import org.antlr.runtime.*;
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+@SuppressWarnings("all")
+public class InternalPascalParser extends AbstractInternalContentAssistParser {
+    public static final String[] tokenNames = new String[] {
+        "<invalid>", "<EOR>", "<DOWN>", "<UP>", "RULE_ID", "RULE_INT", "RULE_STRING", "RULE_ML_COMMENT", "RULE_SL_COMMENT", "RULE_WS", "RULE_ANY_OTHER", "'Hello'", "'!'"
+    };
+    public static final int RULE_ID=4;
+    public static final int RULE_WS=9;
+    public static final int RULE_STRING=6;
+    public static final int RULE_ANY_OTHER=10;
+    public static final int RULE_SL_COMMENT=8;
+    public static final int RULE_INT=5;
+    public static final int T__11=11;
+    public static final int RULE_ML_COMMENT=7;
+    public static final int T__12=12;
+    public static final int EOF=-1;
+
+    // delegates
+    // delegators
+
+
+        public InternalPascalParser(TokenStream input) {
+            this(input, new RecognizerSharedState());
+        }
+        public InternalPascalParser(TokenStream input, RecognizerSharedState state) {
+            super(input, state);
+             
+        }
+        
+
+    public String[] getTokenNames() { return InternalPascalParser.tokenNames; }
+    public String getGrammarFileName() { return "InternalPascal.g"; }
+
+
+    	private PascalGrammarAccess grammarAccess;
+
+    	public void setGrammarAccess(PascalGrammarAccess grammarAccess) {
+    		this.grammarAccess = grammarAccess;
+    	}
+
+    	@Override
+    	protected Grammar getGrammar() {
+    		return grammarAccess.getGrammar();
+    	}
+
+    	@Override
+    	protected String getValueForTokenName(String tokenName) {
+    		return tokenName;
+    	}
+
+
+
+    // $ANTLR start "entryRuleModel"
+    // InternalPascal.g:54:1: entryRuleModel : ruleModel EOF ;
+    public final void entryRuleModel() throws RecognitionException {
+        try {
+            // InternalPascal.g:55:1: ( ruleModel EOF )
+            // InternalPascal.g:56:1: ruleModel EOF
+            {
+            if ( state.backtracking==0 ) {
+               before(grammarAccess.getModelRule()); 
+            }
+            pushFollow(FOLLOW_1);
+            ruleModel();
+
+            state._fsp--;
+            if (state.failed) return ;
+            if ( state.backtracking==0 ) {
+               after(grammarAccess.getModelRule()); 
+            }
+            match(input,EOF,FOLLOW_2); if (state.failed) return ;
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+        }
+        return ;
+    }
+    // $ANTLR end "entryRuleModel"
+
+
+    // $ANTLR start "ruleModel"
+    // InternalPascal.g:63:1: ruleModel : ( ( rule__Model__GreetingsAssignment )* ) ;
+    public final void ruleModel() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalPascal.g:67:2: ( ( ( rule__Model__GreetingsAssignment )* ) )
+            // InternalPascal.g:68:2: ( ( rule__Model__GreetingsAssignment )* )
+            {
+            // InternalPascal.g:68:2: ( ( rule__Model__GreetingsAssignment )* )
+            // InternalPascal.g:69:3: ( rule__Model__GreetingsAssignment )*
+            {
+            if ( state.backtracking==0 ) {
+               before(grammarAccess.getModelAccess().getGreetingsAssignment()); 
+            }
+            // InternalPascal.g:70:3: ( rule__Model__GreetingsAssignment )*
+            loop1:
+            do {
+                int alt1=2;
+                int LA1_0 = input.LA(1);
+
+                if ( (LA1_0==11) ) {
+                    alt1=1;
+                }
+
+
+                switch (alt1) {
+            	case 1 :
+            	    // InternalPascal.g:70:4: rule__Model__GreetingsAssignment
+            	    {
+            	    pushFollow(FOLLOW_3);
+            	    rule__Model__GreetingsAssignment();
+
+            	    state._fsp--;
+            	    if (state.failed) return ;
+
+            	    }
+            	    break;
+
+            	default :
+            	    break loop1;
+                }
+            } while (true);
+
+            if ( state.backtracking==0 ) {
+               after(grammarAccess.getModelAccess().getGreetingsAssignment()); 
+            }
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "ruleModel"
+
+
+    // $ANTLR start "entryRuleGreeting"
+    // InternalPascal.g:79:1: entryRuleGreeting : ruleGreeting EOF ;
+    public final void entryRuleGreeting() throws RecognitionException {
+        try {
+            // InternalPascal.g:80:1: ( ruleGreeting EOF )
+            // InternalPascal.g:81:1: ruleGreeting EOF
+            {
+            if ( state.backtracking==0 ) {
+               before(grammarAccess.getGreetingRule()); 
+            }
+            pushFollow(FOLLOW_1);
+            ruleGreeting();
+
+            state._fsp--;
+            if (state.failed) return ;
+            if ( state.backtracking==0 ) {
+               after(grammarAccess.getGreetingRule()); 
+            }
+            match(input,EOF,FOLLOW_2); if (state.failed) return ;
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+        }
+        return ;
+    }
+    // $ANTLR end "entryRuleGreeting"
+
+
+    // $ANTLR start "ruleGreeting"
+    // InternalPascal.g:88:1: ruleGreeting : ( ( rule__Greeting__Group__0 ) ) ;
+    public final void ruleGreeting() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalPascal.g:92:2: ( ( ( rule__Greeting__Group__0 ) ) )
+            // InternalPascal.g:93:2: ( ( rule__Greeting__Group__0 ) )
+            {
+            // InternalPascal.g:93:2: ( ( rule__Greeting__Group__0 ) )
+            // InternalPascal.g:94:3: ( rule__Greeting__Group__0 )
+            {
+            if ( state.backtracking==0 ) {
+               before(grammarAccess.getGreetingAccess().getGroup()); 
+            }
+            // InternalPascal.g:95:3: ( rule__Greeting__Group__0 )
+            // InternalPascal.g:95:4: rule__Greeting__Group__0
+            {
+            pushFollow(FOLLOW_2);
+            rule__Greeting__Group__0();
+
+            state._fsp--;
+            if (state.failed) return ;
+
+            }
+
+            if ( state.backtracking==0 ) {
+               after(grammarAccess.getGreetingAccess().getGroup()); 
+            }
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "ruleGreeting"
+
+
+    // $ANTLR start "rule__Greeting__Group__0"
+    // InternalPascal.g:103:1: rule__Greeting__Group__0 : rule__Greeting__Group__0__Impl rule__Greeting__Group__1 ;
+    public final void rule__Greeting__Group__0() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalPascal.g:107:1: ( rule__Greeting__Group__0__Impl rule__Greeting__Group__1 )
+            // InternalPascal.g:108:2: rule__Greeting__Group__0__Impl rule__Greeting__Group__1
+            {
+            pushFollow(FOLLOW_4);
+            rule__Greeting__Group__0__Impl();
+
+            state._fsp--;
+            if (state.failed) return ;
+            pushFollow(FOLLOW_2);
+            rule__Greeting__Group__1();
+
+            state._fsp--;
+            if (state.failed) return ;
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__0"
+
+
+    // $ANTLR start "rule__Greeting__Group__0__Impl"
+    // InternalPascal.g:115:1: rule__Greeting__Group__0__Impl : ( 'Hello' ) ;
+    public final void rule__Greeting__Group__0__Impl() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalPascal.g:119:1: ( ( 'Hello' ) )
+            // InternalPascal.g:120:1: ( 'Hello' )
+            {
+            // InternalPascal.g:120:1: ( 'Hello' )
+            // InternalPascal.g:121:2: 'Hello'
+            {
+            if ( state.backtracking==0 ) {
+               before(grammarAccess.getGreetingAccess().getHelloKeyword_0()); 
+            }
+            match(input,11,FOLLOW_2); if (state.failed) return ;
+            if ( state.backtracking==0 ) {
+               after(grammarAccess.getGreetingAccess().getHelloKeyword_0()); 
+            }
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__0__Impl"
+
+
+    // $ANTLR start "rule__Greeting__Group__1"
+    // InternalPascal.g:130:1: rule__Greeting__Group__1 : rule__Greeting__Group__1__Impl rule__Greeting__Group__2 ;
+    public final void rule__Greeting__Group__1() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalPascal.g:134:1: ( rule__Greeting__Group__1__Impl rule__Greeting__Group__2 )
+            // InternalPascal.g:135:2: rule__Greeting__Group__1__Impl rule__Greeting__Group__2
+            {
+            pushFollow(FOLLOW_5);
+            rule__Greeting__Group__1__Impl();
+
+            state._fsp--;
+            if (state.failed) return ;
+            pushFollow(FOLLOW_2);
+            rule__Greeting__Group__2();
+
+            state._fsp--;
+            if (state.failed) return ;
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__1"
+
+
+    // $ANTLR start "rule__Greeting__Group__1__Impl"
+    // InternalPascal.g:142:1: rule__Greeting__Group__1__Impl : ( ( rule__Greeting__NameAssignment_1 ) ) ;
+    public final void rule__Greeting__Group__1__Impl() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalPascal.g:146:1: ( ( ( rule__Greeting__NameAssignment_1 ) ) )
+            // InternalPascal.g:147:1: ( ( rule__Greeting__NameAssignment_1 ) )
+            {
+            // InternalPascal.g:147:1: ( ( rule__Greeting__NameAssignment_1 ) )
+            // InternalPascal.g:148:2: ( rule__Greeting__NameAssignment_1 )
+            {
+            if ( state.backtracking==0 ) {
+               before(grammarAccess.getGreetingAccess().getNameAssignment_1()); 
+            }
+            // InternalPascal.g:149:2: ( rule__Greeting__NameAssignment_1 )
+            // InternalPascal.g:149:3: rule__Greeting__NameAssignment_1
+            {
+            pushFollow(FOLLOW_2);
+            rule__Greeting__NameAssignment_1();
+
+            state._fsp--;
+            if (state.failed) return ;
+
+            }
+
+            if ( state.backtracking==0 ) {
+               after(grammarAccess.getGreetingAccess().getNameAssignment_1()); 
+            }
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__1__Impl"
+
+
+    // $ANTLR start "rule__Greeting__Group__2"
+    // InternalPascal.g:157:1: rule__Greeting__Group__2 : rule__Greeting__Group__2__Impl ;
+    public final void rule__Greeting__Group__2() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalPascal.g:161:1: ( rule__Greeting__Group__2__Impl )
+            // InternalPascal.g:162:2: rule__Greeting__Group__2__Impl
+            {
+            pushFollow(FOLLOW_2);
+            rule__Greeting__Group__2__Impl();
+
+            state._fsp--;
+            if (state.failed) return ;
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__2"
+
+
+    // $ANTLR start "rule__Greeting__Group__2__Impl"
+    // InternalPascal.g:168:1: rule__Greeting__Group__2__Impl : ( '!' ) ;
+    public final void rule__Greeting__Group__2__Impl() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalPascal.g:172:1: ( ( '!' ) )
+            // InternalPascal.g:173:1: ( '!' )
+            {
+            // InternalPascal.g:173:1: ( '!' )
+            // InternalPascal.g:174:2: '!'
+            {
+            if ( state.backtracking==0 ) {
+               before(grammarAccess.getGreetingAccess().getExclamationMarkKeyword_2()); 
+            }
+            match(input,12,FOLLOW_2); if (state.failed) return ;
+            if ( state.backtracking==0 ) {
+               after(grammarAccess.getGreetingAccess().getExclamationMarkKeyword_2()); 
+            }
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__2__Impl"
+
+
+    // $ANTLR start "rule__Model__GreetingsAssignment"
+    // InternalPascal.g:184:1: rule__Model__GreetingsAssignment : ( ruleGreeting ) ;
+    public final void rule__Model__GreetingsAssignment() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalPascal.g:188:1: ( ( ruleGreeting ) )
+            // InternalPascal.g:189:2: ( ruleGreeting )
+            {
+            // InternalPascal.g:189:2: ( ruleGreeting )
+            // InternalPascal.g:190:3: ruleGreeting
+            {
+            if ( state.backtracking==0 ) {
+               before(grammarAccess.getModelAccess().getGreetingsGreetingParserRuleCall_0()); 
+            }
+            pushFollow(FOLLOW_2);
+            ruleGreeting();
+
+            state._fsp--;
+            if (state.failed) return ;
+            if ( state.backtracking==0 ) {
+               after(grammarAccess.getModelAccess().getGreetingsGreetingParserRuleCall_0()); 
+            }
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Model__GreetingsAssignment"
+
+
+    // $ANTLR start "rule__Greeting__NameAssignment_1"
+    // InternalPascal.g:199:1: rule__Greeting__NameAssignment_1 : ( RULE_ID ) ;
+    public final void rule__Greeting__NameAssignment_1() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalPascal.g:203:1: ( ( RULE_ID ) )
+            // InternalPascal.g:204:2: ( RULE_ID )
+            {
+            // InternalPascal.g:204:2: ( RULE_ID )
+            // InternalPascal.g:205:3: RULE_ID
+            {
+            if ( state.backtracking==0 ) {
+               before(grammarAccess.getGreetingAccess().getNameIDTerminalRuleCall_1_0()); 
+            }
+            match(input,RULE_ID,FOLLOW_2); if (state.failed) return ;
+            if ( state.backtracking==0 ) {
+               after(grammarAccess.getGreetingAccess().getNameIDTerminalRuleCall_1_0()); 
+            }
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__NameAssignment_1"
+
+    // Delegated rules
+
+
+ 
+
+    public static final BitSet FOLLOW_1 = new BitSet(new long[]{0x0000000000000000L});
+    public static final BitSet FOLLOW_2 = new BitSet(new long[]{0x0000000000000002L});
+    public static final BitSet FOLLOW_3 = new BitSet(new long[]{0x0000000000000802L});
+    public static final BitSet FOLLOW_4 = new BitSet(new long[]{0x0000000000000010L});
+    public static final BitSet FOLLOW_5 = new BitSet(new long[]{0x0000000000001000L});
+
+}
